51bcf77a46b3c813ef3fa01dfafece10ef6ebfcc
Sharukhali Syed <shahsai11111@gmail.com>
2023-06-07 02:06:41 +0530

    local to remote

--- sub.py
+++ sub.py
@@ -1,7 +1,15 @@
 def sub(a, b):
     return a-b
 
-#going to create a new branch
+
+def mod(a):
+    return a % 2
+
+
+print(mod(10))
+
+# going to create a new branch
+# this is local changes to remote branch created
 a = 10
 b = 20
 print(sub(a, b))
